3e6a24379e86f2048a3391fb7fd64a997fc25106
USUSRIO <USUSRIO@USUSRIO-PC>
2018-09-10 20:08:34 -0400

    inicio del proyecto

---
+++ src/carpeta1/ProgramaUno.java
@@ -0,0 +1,11 @@
+package carpeta1;
+
+public class ProgramaUno {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola gente del curso");
+
+	}
+
+}
---
+++ src/carpeta2/ProgramaDos.java
@@ -0,0 +1,10 @@
+package carpeta2;
+
+public class ProgramaDos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
